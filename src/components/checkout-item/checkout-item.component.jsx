667aae305fee868f2c55ee24b1c6bc08c4f227ad
BuildTools <unconfigured@null.spigotmc.org>
2022-04-28 12:25:14 +1200

    added increment and decrement arrows

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,9 +7,12 @@ import "./checkout-item.styles.scss";
 const CheckoutItem = ({ cartItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
 
-  const { clearItemToCart } = useContext(CartContext);
+  const { clearItemToCart, addItemToCart, removeItemToCart } =
+    useContext(CartContext);
 
   const clearItemHandler = () => clearItemToCart(cartItem);
+  const addItemHandler = () => addItemToCart(cartItem);
+  const removeItemHandler = () => removeItemToCart(cartItem);
 
   return (
     <div className="checkout-item-container">
@@ -17,7 +20,15 @@ const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={removeItemHandler}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={addItemHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={clearItemHandler}>
         &#10005;
