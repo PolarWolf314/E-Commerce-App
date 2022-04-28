ee9cc5f6ed781598513d00d1948bc3fca7d81cd5
BuildTools <unconfigured@null.spigotmc.org>
2022-04-28 14:55:18 +1200

    added category component and styles

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,29 @@
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+
+import "./category.styles.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[categoriesMap]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className="category-ctonainer">
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
+};
+
+export default Category;
