e37ce3283246d211a4fa77c1b5c68fe9db45d9a7
BuildTools <unconfigured@null.spigotmc.org>
2022-04-28 12:33:56 +1200

    finished checkout component and functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -52,12 +52,14 @@ export const CartContext = createContext({
   removeItemToCart: () => {},
   deleteItemToCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     const newCartCount = cartItems.reduce(
@@ -66,6 +68,14 @@ export const CartProvider = ({ children }) => {
     );
     setCartCount(newCartCount);
   }, [cartItems]);
+
+    useEffect(() => {
+      const newCartTotal = cartItems.reduce(
+        (total, cartItem) => total + cartItem.quantity * cartItem.price,
+        0
+      );
+      setCartTotal(newCartTotal);
+    }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
@@ -87,6 +97,7 @@ export const CartProvider = ({ children }) => {
     clearItemToCart,
     cartItems,
     cartCount,
+    cartTotal,
   };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
