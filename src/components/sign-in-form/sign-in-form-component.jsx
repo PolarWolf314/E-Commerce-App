d904f7bfad77bd029933788f2853380a8f9acd93
BuildTools <unconfigured@null.spigotmc.org>
2022-04-29 12:00:06 +1200

    converted button to a styled component

--- src/components/sign-in-form/sign-in-form-component.jsx
+++ src/components/sign-in-form/sign-in-form-component.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import {
   createUserDocumentFromAuth,
@@ -85,7 +85,11 @@ const SignInForm = () => {
         />
         <ButtonsContainer>
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button
+            type="button"
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            onClick={signInWithGoogle}
+          >
             Google sign in
           </Button>
         </ButtonsContainer>
